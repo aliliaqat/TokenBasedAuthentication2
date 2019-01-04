87d974636bb77609f147390dad792c69ef4e5675
Chaudhary Liaqat Ali Kamboh <bzu_ali@yahoo.com>
2019-01-04 09:51:19 +0500

    Initial commit

---
+++ TokenBasedAuthentication2/App_Start/PakistanAuthenticationProvider.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace TokenBasedAuthentication2.App_Start
+{
+    public class PakistanAuthenticationProvider : OAuthAuthorizationServerProvider
+    {
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+            return Task.FromResult<object>(null);
+        }
+        public override Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            context.Validated(new ClaimsIdentity(context.Options.AuthenticationType));
+            return base.GrantResourceOwnerCredentials(context);
+        }
+    }
+}
